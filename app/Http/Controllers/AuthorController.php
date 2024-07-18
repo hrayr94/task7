ce03d5f5d67ab654d01ee9b8de23a37cc0dd83c5
unknown <hrayrashchyan94@gmail.com>
2024-07-18 17:25:21 +0400

    controllers crud

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors = Author::with('books')->paginate(10);
+        return view('author.index', compact('authors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('author.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'biography' => 'required',
+        ]);
+        Author::create($request->only('first_name', 'last_name', 'biography'));
+
+        return redirect()->route('authors.index')->with('success', 'Author created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Author $author)
+    {
+        $author->load('books');
+        return view('author.show', compact('author'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Author $author)
+    {
+        return view('author.edit', compact('author'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Author $author)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'biography' => 'required',
+        ]);
+        $author->update($request->only('first_name', 'last_name', 'biography'));
+        return redirect()->route('authors.index')->with('success', 'Author updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        return redirect()->route('authors.index')->with('success', 'Author deleted successfully.');
+    }
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::with('authors')->paginate(10);
+        return view('books.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $authors = Author::all();
+        return view('books.create', compact('authors'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required',
+            'publication_year' => 'required|integer',
+            'authors' => 'required|array'
+        ]);
+
+        $book = Book::create($request->only('title', 'description', 'publication_year'));
+        $book->authors()->attach($request->authors);
+
+        return redirect()->route('books.index')->with('success', 'Book created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        $book->load('authors');
+        return view('books.show', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        $authors = Author::all();
+        return view('books.edit', compact('book', 'authors'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required',
+            'publication_year' => 'required|integer',
+            'authors' => 'required|array'
+        ]);
+
+        $book->update($request->only('title', 'description', 'publication_year'));
+        $book->authors()->sync($request->authors);
+
+        return redirect()->route('books.index')->with('success', 'Book updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index')->with('success', 'Book deleted successfully');
+    }
+}
